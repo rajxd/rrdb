c313a855a591ac2fa7d3e2ec2495240cc26a8a49
rajxd <rajxdv@gmail.com>
2024-01-11 23:06:25 +0530

    modified main to take basic input in terminal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{stdout, stdin, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello Welcome to rrdb");
+    let mut command = String::new();
+    loop{
+        print!("sdb> ");
+        stdout().flush().unwrap();
+
+        stdin().read_line(&mut command).expect("Error in reading from stdin");
+
+        println!("{}", command);
+        
+        command = "".to_string();
+    }
 }
