29b54ea9d80d8562486eeef74f57545be5e2de77
rajxd <rajxdv@gmail.com>
2024-01-11 23:26:34 +0530

    impl of command types

--- src/commands/command.rs
+++ src/commands/command.rs
@@ -18,4 +18,32 @@ pub enum DbCommand{
     Unknown(String)
 }
 
+impl MetaCommand{
+    pub fn new(cmd: String) -> MetaCommand{
+        match cmd.as_ref(){
+            ".exit" => MetaCommand::Exit,
+            ".tables" => MetaCommand::ListTables,
+            _ => MetaCommand::Unknown(cmd),
 
+        }
+    }
+}
+
+
+impl DbCommand{
+    pub fn new(cmd: String) -> DbCommand{
+        let v = cmd.split(" ").collect::<Vec<&str>>();
+        match v[0]{
+            "create" => DbCommand::CreateTable(cmd),
+            "insert" => DbCommand::Insert(cmd),
+            _ => DbCommand::Unknown(cmd)
+        }
+    }
+}
+
+pub fn get_command_type(cmd: &String) -> CommandType{
+    match cmd.starts_with(".") {
+        true => CommandType::MetaCommand(MetaCommand::new(cmd.to_owned())),
+        false => CommandType::DbCommand(DbCommand::new(cmd.to_owned()))
+    }
+}
